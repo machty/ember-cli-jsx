9ac8ee82428a2df3ed7e079e32b8c23286e8712b
machty <machty@gmail.com>
2014-12-16 14:00:15 -0500

    Got it working

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This addon inserts adds `jsx` processing into the javascript preprocessor for Em
 
 ### Installation / Usage
 
-From within your Ember CLI application (must be > 0.0.34), run the following:
+From within your Ember CLI application (must be >= 0.1.2), run the following:
 
 ```bash
 npm install --save-dev ember-cli-jsx
@@ -12,6 +12,13 @@ npm install --save-dev ember-cli-jsx
 
 That's all. You can now use ES6 syntax supported by `jsx` in your `.jsx`
 files.
+
+### Other things
+
+At some point I should probably expand this into a full on
+`ember-cli-react` addon using trinkets from
+[ember-react](https://github.com/ghempton/ember-react)
+but for now you'll probably want to `bower install react --save`.
 
 ### References
 
--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 'use strict';
 
-var react  = require('broccoli-react');
+var react = require('broccoli-react');
 
-function ReactPlugin(options) {
-  this.name = 'ember-cli-jsx';
-  this.options = options || {};
-  this.ext = this.options.extensions || 'jsx';
-}
+module.exports = {
+  name: 'ember-cli-jsx',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
 
-ReactPlugin.prototype.toTree = function(tree) {
-  return react(tree, this.options);
+    app.registry.add('js', {
+      name: 'ember-cli-jsx',
+      ext: 'jsx',
+      toTree: function(tree) {
+        return react(tree);
+      }
+    });
+  }
 };
 
-function EmberCLIJSX(project) {
-  this.project = project;
-  this.name    = 'Ember CLI JSX';
-}
-
-EmberCLIJSX.prototype.treeFor = function treeFor() {
-};
-
-EmberCLIJSX.prototype.included = function included(app) {
-  var registry = app.registry;
-  this.app = app;
-
-  var plugin = new ReactPlugin(this.app.options.jsxOptions);
-
-  registry.add('jsx', plugin);
-};
-
-module.exports = EmberCLIJSX;
